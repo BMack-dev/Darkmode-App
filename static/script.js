91d90fb9fd13779431024fce902c030fa62991ff
Billy McAdoo <billymcadoo@TheHiddoject103.hsd1.ga.comcast.net>
2024-12-10 17:29:14 -0500

    first commit

---
+++ static/script.js
@@ -0,0 +1,15 @@
+function darkmode(){
+    let body = document.body
+
+    if(body.style.backgroundColor =='white'){
+        body.style.backgroundColor = 'black'
+        body.style.color = 'white'
+
+    }
+    else{
+        body.style.backgroundColor = 'white'
+        body.style.color = 'black'
+
+    }
+
+}
